d8f446f3812386511e9642ba0b47719ed8256596
Curious Paul <170408025@live.unilag.edu.ng>
2020-03-26 12:46:32 +0100

    finished day0, and day1

---
+++ app.py
@@ -0,0 +1,37 @@
+# 30 DAYS OF CODE
+import datetime as d
+
+# Date viewer (DAY 0)
+def dateview():
+    diff = d.timedelta(0,0,0,0,0,1,0)
+    date = diff + d.datetime.utcnow()
+    date_ = str(date)
+    time = date_.split(' ')
+    #complements = ['beautiful','wonderful','gracious','magnificent']
+    print(f"It's a gracious {date.strftime('%A')}")
+    print(f"Current time is {time[1]} {date.strftime('%p')}")
+
+    pass
+
+
+# Number Checker (DAY 1)
+def checker(*args):
+    if len(args) == 1:
+        if args[0] < 0:
+            return f"{args[0]} is Negative"
+        elif args[0] > 0:
+            return f"{args[0]} is Positive"
+    
+    if len(args) > 1:
+        if args[0] > 0 and args[1] > 0:
+            return f"{args[0]} is Positive, {args[1]} is Positive"
+        elif args[0] > 0 and args[1] < 0:
+            return f"{args[0]} is Positive, {args[1]} is Negative"
+        elif args[0] < 0 and args[1] < 0:
+            return f"{args[0]} is Negative, {args[1]} is Negative"
+        elif args[0] < 0 and args[1] > 0:
+            return f"{args[0]} is Negative, {args[1]} is Positive"
+
+
+print(checker(-2,4))
+dateview()
