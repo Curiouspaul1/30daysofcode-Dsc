c8f88c86eb0481324d0adf19b920f3fb758a30f6
Curious Paul <170408025@live.unilag.edu.ng>
2020-03-30 12:31:39 +0100

    added new file for day2

---
+++ Day2.py
@@ -0,0 +1,15 @@
+# Type check (Day 2)
+def typecheck(*args):
+    if len(args) != 0:
+        typeof = type(args[0])
+        if typeof:
+            return typeof
+        else:
+            return "Error"
+    else:
+        return "Error"
+
+try:
+    print(typecheck())
+except SyntaxError or TypeError or NameError or ValueError:
+    raise "Error"
