156e0d38a21b29c2f2232c36a6a066b2d72b4646
Curious Paul <170408025@live.unilag.edu.ng>
2020-04-07 13:32:43 +0100

    day14

--- day13.py
+++ day13.py
@@ -37,4 +37,4 @@ def get():
     return make_response(jsonify({"payload":data}),200)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
---
+++ day14.py
@@ -0,0 +1,53 @@
+from flask import Flask,jsonify,make_response,request
+
+app = Flask(__name__)
+
+# Native function
+def palindromecheck(str):
+    """
+        palindromecheck is a fullproof palindrome detector 
+        detects single word palindromes such as eve or 
+        multiple word palindromes such as 'nurses run'\
+        
+        args: <class 'string'>
+        returns: <class 'bool'>
+
+        PS: this function is used in the server handler below to perdorm the
+        check on received input from external applications
+    """
+    list1 = str.split(' ')
+    if len(list1)==1:
+        word = str[::-1]
+        if word == str:
+            return True
+        else:
+            return False
+        pass
+    else:
+        placehold = []
+        placehold1 = []
+        for i in range(1,len(list1)+1):
+            placehold.append(list1[-i])
+        for i in placehold:
+            placehold1.append(i[::-1])
+        
+        word = ''.join(i for i in placehold1)
+        if word == ''.join(i for i in list1):
+            return True
+        else:
+            return False
+
+# Server Handler
+@app.route('/drome',methods=['GET'])
+def dromer():
+    #data = request.get_json()
+    words = request.get_json()['words']
+    dict_ = []
+    for word in words:
+        if palindromecheck(word):
+            dict_.append(word)
+    return make_response(jsonify({"words":dict_}),200)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
